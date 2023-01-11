a614d7572e6d965e2999eafac52f9ab733e0aa5a
Alan Fung-Schwarz <alan@atomsign.net>
2023-01-11 15:39:10 -0800

    Escape string

--- lua/rg-nvim/init.lua
+++ lua/rg-nvim/init.lua
@@ -1,9 +1,11 @@
-local Ack = function (opts)
+Ack = function (opts)
   if opts.fargs[2] == nil then
     opts.fargs[2] = './'
   end
 
-  local cmd = string.format("cgetexpr system('%s %s %s')", vim.g.ackprg, opts.fargs[1], opts.fargs[2])
+  local str_query = vim.fn.shellescape(opts.fargs[1])
+  local dir = vim.fn.shellescape(opts.fargs[2])
+  local cmd = string.format("cgetexpr system('%s %s %s')", vim.g.ackprg, str_query, dir)
   vim.cmd(cmd)
   vim.cmd("copen")
 
