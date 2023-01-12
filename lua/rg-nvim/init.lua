7506d0e1eb0f6bacf93f146af258ef93d728072d
Alan Fung-Schwarz <alan@atomsign.net>
2023-01-11 16:38:42 -0800

    Prompt install of ripgrep

--- lua/rg-nvim/init.lua
+++ lua/rg-nvim/init.lua
@@ -1,11 +1,20 @@
+InstallRg = require('install-rg')
+
 Ack = function (opts)
+  local is_installed = InstallRg()
+  if is_installed == false then return false end
+
   if opts.fargs[2] == nil then
     opts.fargs[2] = './'
   end
 
+  if vim.g.rgprg == nil then
+    vim.g.rgprg = "rg --vimgrep --smartcase"
+  end
+
   local str_query = vim.fn.shellescape(opts.fargs[1])
   local dir = vim.fn.shellescape(opts.fargs[2])
-  local cmd = string.format("cgetexpr system('%s %s %s')", vim.g.ackprg, str_query, dir)
+  local cmd = string.format("cgetexpr system('%s %s %s')", vim.g.rgprg, str_query, dir)
   vim.cmd(cmd)
   vim.cmd("copen")
 
---
+++ lua/rg-nvim/install-rg.lua
@@ -0,0 +1,31 @@
+function InstallRg()
+  if vim.fn.executable('rg') == 1 then
+    if vim.fn.executable('brew') == 0 then
+      local answer, install, yeses = nil, false, {"y", "yes"}
+      vim.ui.input({
+          prompt = "You don't seem to have ripgrep executable. Install by homebrew? (y/N) "
+        },
+        function(input) answer = string.lower(input) end
+        )
+      for _,val in ipairs(yeses) do
+        if val == answer then
+          install = true
+        end
+      end
+
+      if install == true then
+        vim.cmd("exec ':!brew install ripgrep'")
+      end
+
+      return true
+    else
+      print("ripgrep is not install and package manager is not known")
+    end
+
+    -- Don't know how to deal with not having homebrew
+    return false
+  end
+
+  -- ripgrep is install, so just move on
+  return true
+end
